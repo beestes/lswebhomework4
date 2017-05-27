55c47343ed30aec7a6a2826c4f0e35885801a94d
Blake Estes <blakee.estes90@gmail.com>
2017-05-26 23:27:18 -0500

    troubleshooting background color

---
+++ script.js
@@ -0,0 +1,17 @@
+document.body.style.backgroundColor = "#ffe6e6";
+
+document.querySelector("h1").style.backgroundColor = "#e6f2ff";
+
+document.body.style.fontFamily = "sans-serif";
+
+document.getElementById("nickname").innerHTML = "Blake";
+document.getElementById("favorites").innerHTML = "Typography, Design, Photography, Creating, Music";
+document.getElementById("hometown").innerHTML = "Saint Louis";
+
+document.querySelector("li").className = "listitem";
+document.getElementsByClassName("listitem").style.backgroundColor = "#ffb3b3";
+
+var me = document.createElement('img');
+me.src = "pictures/me.jpg";
+
+document.body.appendChild(me);
