21b740314d03b6175ac9c630825d60efe24da964
Blake Estes <blakee.estes90@gmail.com>
2017-05-27 00:29:44 -0500

    look is correct still working on changing class

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ document.getElementById("nickname").innerHTML = "Blake";
 document.getElementById("favorites").innerHTML = "Typography, Design, Photography, Creating, Music";
 document.getElementById("hometown").innerHTML = "Saint Louis";
 
-document.querySelector("li").className = "listitem";
-document.getElementsByClassName("listitem").style.backgroundColor = "#ffb3b3";
+//document.querySelector("li").className = "listitem";
+document.querySelector("ul").style.backgroundColor = "#ffb3b3";
 
 var me = document.createElement('img');
 me.src = "pictures/me.jpg";
